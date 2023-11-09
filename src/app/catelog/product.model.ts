81e28c4e49c032c035dac976984c9d2ebf879626
Maria Groeizaam <maria.groeizaam@centric.eu>
2023-11-09 10:38:50 +0100

    Angular directives

---
+++ src/app/catelog/product.model.ts
@@ -0,0 +1,10 @@
+export interface IProduct{
+    id: number;
+    description: string;
+    name:string;
+    imageName:string;
+    category:string;
+    price:number;
+    discount:number;
+}
+//Do you see typeschrip is just like c# strongly typed! Use constructor in component to activate
