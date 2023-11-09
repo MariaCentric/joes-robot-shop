a9eb4184a9d8d93aba9539bbfa2a59a44f084a26
Maria Groeizaam <maria.groeizaam@centric.eu>
2023-11-09 09:47:16 +0100

    Setup JavaScript Interpolation Angular

---
+++ src/app/catelog/catelog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catelog',
+  templateUrl: './catelog.component.html',
+  styleUrls: ['./catelog.component.css']
+})
+export class CatelogComponent {
+
+}
